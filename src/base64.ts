761a88697c15ae2c4f6c0896053c94538e378f33
AoEiuV020 <aoeiuv020@gmail.com>
2025-04-29 09:29:42 +0800

    base64中改用BinString处理字符串

--- src/base64.ts
+++ src/base64.ts
@@ -1,11 +1,12 @@
 import forge from "node-forge";
+import { BinString } from "./bin";
 
 export class Base64 {
   static encode(content: Uint8Array): string {
-    return forge.util.encode64(forge.util.binary.raw.encode(content));
+    return forge.util.encode64(BinString.toString(content));
   }
 
   static decode(content: string): Uint8Array {
-    return forge.util.binary.raw.decode(forge.util.decode64(content));
+    return BinString.toByteArray(forge.util.decode64(content));
   }
 }

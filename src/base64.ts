0dea05ad7bb05fe6ad10203300796139fe61cfdd
AoEiuV020 <aoeiuv020@gmail.com>
2025-05-22 16:59:41 +0800

    修改base64使用forge
    之前是以为鸿蒙没法使用forge才更换了实现的，

--- src/base64.ts
+++ src/base64.ts
@@ -1,11 +1,12 @@
-import { fromByteArray, toByteArray } from "base64-js";
+import forge from "node-forge";
+import { BinString } from "./bin";
 
 export class Base64 {
   static encode(content: Uint8Array): string {
-    return fromByteArray(content);
+    return forge.util.encode64(BinString.toString(content));
   }
 
   static decode(content: string): Uint8Array {
-    return toByteArray(content);
+    return BinString.toByteArray(forge.util.decode64(content));
   }
 }
